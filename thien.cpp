cb2b0253e79fe3e13a332c6ea59adf8d4db8eda6
thienkhanh2k3 <91043602+thienkhanh2k3@users.noreply.github.com>
2021-11-02 21:12:33 +0700

    Add files via upload

---
+++ thien.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+    int a,b,tong,hieu;
+    printf("nhap so a :");
+    scanf("%d",&a);
+    printf("nhap so b:");
+    scanf("%d",&b);
+    tong=a+b;
+    hieu=a-b;
+
+    printf("tong hai so la:%d\n",tong);
+    printf("hieu hai so la:%d",hieu);
+    
+    return 0;
+}
